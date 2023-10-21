8e1d58d3feab9472df4845fb6ebf31f416ee9737
Vadim Kiniabaev <thekinv21@gmail.com>
2023-10-21 17:49:42 +0300

    FEAT: PORJECT INITHILIZED

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule)
+
+	// burada  http://localhost::4200/api olmasını sağladık
+	await app.setGlobalPrefix('api')
+
+	// hangi portta çalışmasını ve o portu dinlemesini söyledik
+	await app.listen(4200)
+
+	// cors işlemleri için
+	app.enableCors()
 }
-bootstrap();
+bootstrap()
