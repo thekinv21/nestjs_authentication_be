099c1e8dba2de18a46abf89c8c086a89c851561b
Vadim Kiniabaev <thekinv21@gmail.com>
2023-10-22 23:36:07 +0300

    FEAT: CREATE LOGİN AND GETNEWTOKEN CONTROLLERS

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -26,6 +26,7 @@ import {
 } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { AuthDto } from './dto/auth.dto'
+import { RefreshTokenDto } from './dto/refresh-token.dto'
 
 @Controller('auth')
 export class AuthController {
@@ -38,5 +39,19 @@ export class AuthController {
 	@Post('register')
 	async register(@Body() dto: AuthDto) {
 		return this.authService.register(dto)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post('login')
+	async login(@Body() dto: AuthDto) {
+		return this.authService.login(dto)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post('login/access-token')
+	async getNewTokens(@Body() dto: RefreshTokenDto) {
+		return this.authService.getNewTokens(dto.refreshToken)
 	}
 }
