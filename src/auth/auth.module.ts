3834164215cd4469dd0e5a770fb20dc01c4c4206
Vadim Kiniabaev <thekinv21@gmail.com>
2023-10-22 18:25:21 +0300

    FETA: CREATE  AUTH MODULE

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,11 +7,24 @@
 //? @Module dekoratorü oluştur ve içine de obje oluştur, obje içinde de imports, providers, controllerları yerleştir daha sonra classı export et
 
 import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtModule, JwtService } from '@nestjs/jwt'
+import { PrismaService } from 'prisma.service'
+import { getJwtConfig } from 'src/config/jwt.config'
+import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
+import { JwtStrategy } from './strategy/jwt.strategy'
 
 @Module({
-	imports: [],
-	controllers: [],
-	providers: [AuthService]
+	controllers: [AuthController],
+	providers: [AuthService, PrismaService, JwtStrategy, JwtService],
+	imports: [
+		ConfigModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJwtConfig
+		})
+	]
 })
 export class AuthModule {}
