8a04e89ae89169ec3c98e1552b9798157c3d2175
Vadim Kiniabaev <thekinv21@gmail.com>
2023-10-22 18:27:58 +0300

    FETA: ADDED ALL MODULES TO MAİN MODULE

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { JwtService } from '@nestjs/jwt'
+import { PrismaService } from 'prisma.service'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthController } from './auth/auth.controller'
+import { AuthModule } from './auth/auth.module'
 import { AuthService } from './auth/auth.service'
 
 @Module({
-	imports: [],
+	imports: [AuthModule, ConfigModule.forRoot()],
 	controllers: [AppController, AuthController],
-	providers: [AppService, AuthService]
+	providers: [AppService, AuthService, PrismaService, JwtService]
 })
 export class AppModule {}
